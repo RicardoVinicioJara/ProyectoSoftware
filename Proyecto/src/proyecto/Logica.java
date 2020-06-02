1eec6479ee7f27897a1e6816ac151ca2398df63f
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-06-01 23:20:52 -0500

    Initial commit

---
+++ Proyecto/src/proyecto/Logica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Vinicio
+ */
+public class Logica {
+    public String RicardoJara(){
+        return "Este metodo a sido modificado por: Ricardo Jara ";
+    }
+    public String JuanCañar(){
+        return "Juan Cañar ";
+    }
+    
+    public String FannyGutama(){
+        return "Fanny Gutama ";
+    }
+}
---
+++ Proyecto/src/proyecto/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Vinicio
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Logica logica = new Logica();
+        System.out.println("Metodo Primer Integrante");
+        System.out.println(logica.RicardoJara());
+        System.out.println("Metodo Segundo Integrante");
+        System.out.println(logica.JuanCañar());
+        System.out.println("Metodo Tercer Integrante");
+        System.out.println(logica.FannyGutama());
+        
+    }
+    
+}
